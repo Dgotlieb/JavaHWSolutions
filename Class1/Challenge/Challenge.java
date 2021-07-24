ab76568fbff1a7e73f010c683e425f8d349d1dd7
dgotlieb <Dgotlieb8@gmail.com>
2021-07-24 14:33:54 +0300

    Add files via upload

---
+++ Class1/Challenge/Challenge.java
@@ -0,0 +1,15 @@
+public class Challenge {
+    public static void main(String[] args) {
+        //10
+        String s = "123";
+        System.out.println(Integer.parseInt(s) / 2);
+
+        //11
+        // String a2 = new String("text"); explicitly creates a new distinct instance of a String object;
+        // String a1 = "a"; may reuse an instance from the string constant pool if one is available.
+
+        //12
+        // The naming convention in java calls classes to start with a capital letter.
+        // String is a class while the other 8 are primitives.
+    }
+}
