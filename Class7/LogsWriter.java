e00d05a1f68e396eb91a04ee94dd76cf1c874a76
dgotlieb <Dgotlieb8@gmail.com>
2021-08-02 09:25:25 +0300

    Update LogsWriter.java

--- Class7/LogsWriter.java
+++ Class7/LogsWriter.java
@@ -4,19 +4,19 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 
 public class LogsWriter {
-    private static exrcesises.class8.LogsWriter instance = null;
+    private static LogsWriter instance = null;
 
     private LogsWriter() {}
 
-    public static exrcesises.class8.LogsWriter getInstance() {
+    public static LogsWriter getInstance() {
         if (instance == null) {
-            instance = new exrcesises.class8.LogsWriter();
+            instance = new LogsWriter();
         }
         return instance;
     }
 
     public void writeToFile(String content) throws IOException {
-        String filePath = "C:\\Users\\user\\log.txt";
-        Files.writeString(Path.of(filePath), content + "\n", StandardOpenOption.APPEND);
+        String filePath = "log.txt";
+        Files.writeString(Path.of(filePath), content + "\n", StandardOpenOption.CREATE_NEW);
     }
 }
