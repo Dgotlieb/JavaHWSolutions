a0e6affbde7f5b039571534bb98bbe8ba7a12398
dgotlieb <Dgotlieb8@gmail.com>
2021-08-05 20:53:03 +0300

    Update Challenge.java

--- Class8/Challenge/Challenge.java
+++ Class8/Challenge/Challenge.java
@@ -5,12 +5,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Challenge {
     public static void main(String[] args) {
-        //15
+        //9
         ChromeDriver driver = new ChromeDriver();
         WebElement element = driver.findElement(By.id("hello"));
         ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
 
-        //16
+        //10
         // Yes, Selenium Grid
     }
 }
